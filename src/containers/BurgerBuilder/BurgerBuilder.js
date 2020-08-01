89b5e79b1b1c702571c3f54da5783da06be056de
Nakahotty <naskuuu@abv.bg>
2020-08-02 00:40:20 +0300

    Added OrderSummary component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import Model from '../../components/UI/Model/Model';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -98,6 +101,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Model>
+                    <OrderSummary ingredients={this.state.ingredients}/>    
+                </Model> 
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler} 
