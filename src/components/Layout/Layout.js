ad0d7f9b3c7ec8cd13842e9d3ef8d02b92140778
Nakahotty <naskuuu@abv.bg>
2020-09-23 21:13:19 +0300

    Added nav items and side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
+
 
 // Using wrapper high-order component
 const layout = (props) => (
     <Aux>
         <Toolbar /> 
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
